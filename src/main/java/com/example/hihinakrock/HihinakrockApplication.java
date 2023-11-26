df90706a418cd60ee41d080b6cbff39ee0cca4e4
nakgyeom <himina1227@gmail.com>
2023-11-26 10:15:37 +0900

    - first commit

---
+++ src/main/java/com/example/hihinakrock/HihinakrockApplication.java
@@ -0,0 +1,13 @@
+package com.example.hihinakrock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HihinakrockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HihinakrockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hihinakrock/HihinakrockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hihinakrock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HihinakrockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
