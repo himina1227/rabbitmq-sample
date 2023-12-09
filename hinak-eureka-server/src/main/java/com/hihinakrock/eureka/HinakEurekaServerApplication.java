90efbade3749997f2fbba6c110a069bc66b8c076
nakgyeom <himina1227@beplushealthcare.com>
2023-12-09 23:59:57 +0900

    - eureka-server 추가

---
+++ hinak-eureka-server/src/main/java/com/hihinakrock/eureka/HinakEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.hihinakrock.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HinakEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HinakEurekaServerApplication.class, args);
+    }
+
+}
---
+++ hinak-eureka-server/src/test/java/com/hihinakrock/eureka/HinakEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hihinakrock.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HinakEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
