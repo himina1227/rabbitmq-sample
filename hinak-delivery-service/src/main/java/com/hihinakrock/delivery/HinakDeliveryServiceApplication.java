0d017ee13a55b56e57ac063715a7a1ac7545c75e
nakgyeom <himina1227@beplushealthcare.com>
2023-12-10 00:04:09 +0900

    - hinak-delivery-service 모듈 추가

---
+++ hinak-delivery-service/src/main/java/com/hihinakrock/delivery/HinakDeliveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hihinakrock.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HinakDeliveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HinakDeliveryServiceApplication.class, args);
+    }
+
+}
---
+++ hinak-delivery-service/src/test/java/com/hihinakrock/delivery/HinakDeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hihinakrock.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HinakDeliveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
